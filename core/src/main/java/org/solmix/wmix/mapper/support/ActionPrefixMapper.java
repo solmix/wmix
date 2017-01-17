4018830f71bbe7fce64bf4f97436b60971f09529
solmix.f <solmix.f@gmail.com>
2017-01-17 17:56:07 +0800

    mapper

---
+++ core/src/main/java/org/solmix/wmix/mapper/support/ActionPrefixMapper.java
@@ -0,0 +1,57 @@
+package org.solmix.wmix.mapper.support;
+
+import org.solmix.commons.util.ArrayUtils;
+import org.solmix.commons.util.StringUtils;
+
+public class ActionPrefixMapper extends AbstractMapper {
+
+	private static final  String type="action";
+	
+	private String prefix;
+
+	@Override
+	public String getType() {
+		return type;
+	}
+
+	public static final String TEMPLATE_NAME_SEPARATOR = ".";
+	public static final String NAME_SEPARATOR = ";";
+
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	@Override
+	public Boolean isCacheEnabled() {
+		return false;
+	}
+
+	@Override
+	protected boolean isCacheEnabledByDefault() {
+		return false;
+	}
+
+	@Override
+	public String doMapping(String name) {
+		String[] parts = StringUtils.split(name, NAME_SEPARATOR);
+
+		if (ArrayUtils.isEmptyArray(parts)) {
+			throw new IllegalArgumentException("mapping name is empty");
+		}
+
+		boolean withPrefix = !StringUtils.isEmpty(getPrefix());
+		String firstTemplateName = StringUtils.join(parts,TEMPLATE_NAME_SEPARATOR);
+
+		if (withPrefix) {
+			firstTemplateName = getPrefix() + TEMPLATE_NAME_SEPARATOR
+					+ firstTemplateName;
+		}
+
+		return firstTemplateName;
+	}
+}
